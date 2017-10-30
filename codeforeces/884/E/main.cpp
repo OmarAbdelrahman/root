208ff2b8a2843521d7ec5bc595620d1179e4029e
Omar Saber <omar.saber@ligadigital.com>
2017-10-30 18:08:36 +0100

    Binary Matrix less memory implementation

--- codeforeces/884/E/main.cpp
+++ codeforeces/884/E/main.cpp
@@ -3,91 +3,75 @@ using namespace std;
 
 const int M = (1 << 14) + 1;
 
-struct disjoint_set {
-  int* parent;
-
-  disjoint_set() { }
-
-  disjoint_set(int n) {
-    initialize(n);
-  }
-
-  void initialize(int n) {
-    parent = new int[n];
-    for (int i = 0; i < n; i++) {
-      parent[i] = i;
-    }
-  }
-
-  int operator [](int x) {
-    if (parent[x] == x) return x;
-    return (parent[x] = operator[](parent[x]));
-  }
-
-  void merge(int a, int b) {
-    a = (*this)[a];
-    b = (*this)[b];
-    if (a == b) {
-      return;
-    }
-    if (a < b) {
-      parent[a] = b;
-    } else {
-      parent[b] = a;
-    }
-  }
-
-  ~disjoint_set() {
-    delete[] parent;
-  }
-};
-
 inline int from_hex(char c) {
   if (isdigit(c)) return c - '0';
   return c - 'A' + 10;
 }
 
 char column[M];
-bitset<M> row;
-bitset<M> top;
+int parent[M * 2];
+
+inline int fs(int x) {
+  if (parent[x] == x) return x;
+  return (parent[x] = fs(parent[x]));
+}
+
+void merge(int a, int b) {
+  a = fs(a);
+  b = fs(b);
+  if (a == b) {
+    return;
+  }
+  if (a < b) {
+    parent[b] = a;
+  } else {
+    parent[a] = b;
+  }
+}
 
 int main() {
   int n, m;
   cin >> n >> m;
-  const int ms = m / 4;
-  disjoint_set ds(m * 2);
+  for (int i = 0; i < 2 * m; i++) {
+    parent[i] = -1;
+  }
   int result = 0;
   for (int i = 0; i < n; i++) {
     scanf("%s", column);
-    for (int j = 0; j < ms; j++) {
-      int value = from_hex(column[j]);
-      for (int k = 0; k < 4; k++) {
-        row[j * 4 + k] = (value >> (3 - k)) & 1;
-      }
-    }
     for (int j = 0; j < m; j++) {
-      ds.parent[j + m] = j + m;
+      if (parent[j] != -1) {
+        parent[j + m] = parent[j] + m;
+      } else {
+        parent[j + m] = -1;
+      }
+      parent[j] = -1;
     }
+    bool left = false;
     for (int j = 0; j < m; j++) {
-      if (j > 0 && row[j - 1] && row[j]) {
-        ds.merge(j + m - 1, j + m);
+      const int index = j / 4;
+      bool cur_bit = (from_hex(column[index]) >> ((j & 3) ^ 3)) & 1;
+      if (cur_bit) {
+        parent[j] = j;
+        if (left) {
+          merge(j, j - 1);
+        }
+        if (parent[j + m] != -1) {
+          merge(j, j + m);
+        }
+      } else {
+        parent[j] = -1;
       }
-      if (row[j] && top[j]) {
-        ds.merge(j, j + m);
+      left = cur_bit;
+    }
+    for (int j = m; j < 2 * m; j++) {
+      if (parent[j] == j) {
+        result++;
       }
     }
-    for (int j = 0; j < m; j++) {
-      if (top[j] && ds[j] == j) {
-        ++result;
-      }
-      int row_parent = ds[j + m];
-      ds.parent[j] = row_parent != j + m ? row_parent - m : j;
-    }
-    top = row;
   }
-  for (int j = 0; j < m; j++) {
-    if (top[j] && ds[j] == j) {
-      ++result;
+  for (int i = 0; i < m; i++) {
+    if (parent[i] == i) {
+      result++;
     }
   }
   cout << result << endl;
