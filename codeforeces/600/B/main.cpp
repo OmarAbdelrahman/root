03cf94bfbeaf320e52136fae7e64172ae2fd592d
Omar Saber <omar.saber@ligadigital.com>
2017-12-14 18:28:44 +0100

    B solution

--- codeforeces/600/B/main.cpp
+++ codeforeces/600/B/main.cpp
@@ -3,35 +3,36 @@ using namespace std;
 
 template<class T>
 inline T next() {
-  T x;
-  cin >> x;
-  return x;
+    T x;
+    cin >> x;
+    return x;
 }
 
 int f(int elem, const vector<int>& a) {
-  int s = 0;
-  int e = a.size() - 1;
-  while (s < e) {
-    int m = (s / 2) + (e / 2);
-    if (a[m] <= elem) {
-      s = m + 1;
-    } else {
-      e = m - 1;
+    int s = 0;
+    int e = a.size();
+    while (s < e) {
+        int m = (s / 2) + (e / 2);
+        if (a[m] <= elem) {
+            s = m + 1;
+        } else {
+            e = m;
+        }
     }
-  }
-  return s;
+    return s;
 }
 
 int main() {
-  int n, m;
-  cin >> n >> m;
-  vector<int> a(n), b(m);
-  for (int& it : a) cin >> it;
-  for (int& it : b) cin >> it;
-  sort(a.begin(), a.end());
-  for (int i : b) {
-    cout << f(i, a) << " ";
-  }
-  cout << endl;
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n), b(m);
+    for (int& it : a) cin >> it;
+    for (int& it : b) cin >> it;
+    sort(a.begin(), a.end());
+    for (int i = 0; i < m; i++) {
+        if (i > 0) cout << " ";
+        cout << f(b[i], a);
+    }
+    cout << endl;
   return 0;
 }
