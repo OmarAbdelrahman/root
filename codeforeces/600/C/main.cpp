7d90959bba73097226078a937b7b8fe1e832a176
Omar Saber <omar.saber@ligadigital.com>
2017-12-18 14:54:22 +0100

    Make Palindrome

--- codeforeces/600/C/main.cpp
+++ codeforeces/600/C/main.cpp
@@ -2,5 +2,42 @@
 using namespace std;
 
 int main() {
+  string s;
+  cin >> s;
+  unordered_map<char, int> f;
+  for (char ch : s) {
+    f[ch]++;
+  }
+  const int size = s.length();
+  for (char ch1 = 'a'; ch1 <= 'z'; ch1++) {
+    if (f[ch1] % 2 == 0) continue;
+    for (char ch2 = 'z'; ch2 >= 'a'; ch2--) {
+      if (f[ch2] % 2 == 0 || ch1 == ch2) continue;
+      if (ch1 < ch2) {
+        f[ch1]++;
+        f[ch2]--;
+      } else {
+        f[ch1]--;
+        f[ch2]++;
+      }
+      break;
+    }
+  }
+  int index = 0;
+  for (char ch = 'a'; ch <= 'z'; ch++) {
+    while (f[ch] >= 2) {
+      if (index < size / 2) {
+        s[index] = s[size - index - 1] = ch;
+        index++;
+        f[ch] -= 2;
+      }
+    }
+  }
+  for (char ch = 'a'; ch <= 'z'; ch++) {
+    if (f[ch] % 2 != 0) {
+      s[size / 2] = ch;
+    }
+  }
+  cout << s << endl;
   return 0;
 }
