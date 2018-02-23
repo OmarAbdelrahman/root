84679a033d1f410c18316a407360dd96bfbc9a4b
Omar Saber <omar.saber@ligadigital.com>
2018-02-23 15:29:15 +0100

    prepare Edges in MST solution

--- codeforeces/home_work_eight_mst/C/main.cpp
+++ codeforeces/home_work_eight_mst/C/main.cpp
@@ -1,69 +1,68 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const char* msg[] = {
+    "none",
+    "any",
+    "at least one"
+};
+
 struct edge {
-    int f, t, c, id;
-    
-    edge(int _f, int _t, int _c, int _id): f(_f), t(_t), c(_c), id(_id) { }
-    
+    int f, t, c, i, ty = -1;
+
     bool operator < (const edge& e) const {
         return c < e.c;
     }
 };
 
-struct disjoin_set {
-    disjoin_set(int n): parent(n), rank(n) {
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
-            rank[i] = 0;
-        }
+struct disjoint_set {
+    disjoint_set(int n): p(n), rnd(n) {
+        for (int i = 0; i < n; i++) p[i] = i;
     }
-    
-    int find(int a) {
-        return parent[a] == a ? a : (parent[a] = find(parent[a]));
+
+    int find(int x) {
+        return p[x] == x ? x : p[x] = find(p[x]);
     }
-    
-    bool merge(int a, int b) {
+
+    bool join(int a, int b) {
         a = find(a);
         b = find(b);
         if (a == b) return false;
-        if (rank[a] > rank[b])
-            swap(a, b);
-        parent[a] = b;
-        rank[b] += int(rank[a] == rank[b]);
+        if (rnd() % 2 == 0) {
+            p[a] = b;
+        } else {
+            p[b] = a;
+        }
         return true;
     }
-    
-    vector<int> parent;
-    vector<int> rank;
+
+private:
+    vector<int> p;
+    mt19937 rnd;
 };
 
-vector<edge> solve(int n, vector<int>& cnt, vector<edge>& es) {
-    disjoin_set s(n);
+void process_edges(vector<edge>& es) {
+    const int n = es.size();
     sort(begin(es), end(es));
+    for (int i = 0; i < n; ) {
+
+    }
 }
 
 int main() {
     int n, m;
     scanf("%d%d", &n, &m);
-    
-    vector<edge> edges;
+    vector<edge> es(m);
     for (int i = 0; i < m; i++) {
-        int a, b, c;
-        scanf("%d%d%d", &a, &b, &c);
-        edges.push_back(edge(a - 1, b - 1, c, i));
+        int f, t, c;
+        scanf("%d%d%d", &f, &t, &c);
+        es[i] = {f - 1, t - 1, c, i};
     }
-    
-    vector<int> cnt(m, 0);
-    vector<edge> e1 = solve(n, cnt, edges);
-    for (const auto& e : e1) {
-        solve_again(n, e.id, cnt, edges);
-    }
-    
-    for (int i = 0; i < m; i++) {
-        int c = cnt[i];
-        if (c == 0) puts("none");
-        else if (c == 1) puts("at least one");
-        else puts("any");
+    process_edges(es);
+    sort(begin(es), end(es), [](const edge& e1, const edge& e2) {
+        return e1.i < e2.i;
+    });
+    for (const auto& e : es) {
+        puts(msg[e.i]);
     }
 }
