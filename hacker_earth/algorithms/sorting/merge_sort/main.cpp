cc51a968a428ab068c353e6bff5c27fee37e0fd1
Omar Saber <omar.saber@ligadigital.com>
2018-02-27 14:21:00 +0100

    partial solution

--- hacker_earth/algorithms/sorting/merge_sort/main.cpp
+++ hacker_earth/algorithms/sorting/merge_sort/main.cpp
@@ -5,35 +5,35 @@ const int N = 1000000;
 
 int a[N];
 
-int sort(int b, int e) {
+long long sort(int b, int e) {
     if (b >= e) return 0;
 
     int m = (b + e) / 2;
-    int ls = sort(b, m);
-    int rs = sort(m + 1, e);
+    long long ls = sort(b, m);
+    long long rs = sort(m + 1, e);
 
     int* t = new int[e - b + 1];
-    int jl = m;
+    int jl = b;
     int jr = m + 1;
-    int ti = e - b;
+    int ti = 0;
 
-    int res = ls + rs;
+    long long res = 0;
     for (int i = b; i <= e; i++) {
-        if (jl < b) {
-            t[ti--] = a[jr++];
+        if (jl > m) {
+            t[ti++] = a[jr++];
         } else if (jr > e) {
-            t[ti--] = a[jl--];
+            t[ti++] = a[jl++];
         } else if (a[jl] > a[jr]) {
-            t[ti--] = a[jr++];
-            res += 1;
+            t[ti++] = a[jr++];
+            res += (m -  jl + 1LL);
         } else {
-            t[ti--] = a[jl--];
+            t[ti++] = a[jl++];
         }
     }
-    for (int i = 0, ai = b; i < e - b + 1; i++) {
+    for (int i = 0, ai = b; i < ti; i++) {
         a[ai++] = t[i];
     }
-    return res;
+    return res + rs + ls;
 }
 
 int main() {
@@ -44,10 +44,6 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
-
-    for (int i = 0; i < n; i++)
-        cout << a[i] << " \n"[i == n - 1];
-
     int swaps = sort(0, n - 1);
     cout << swaps << '\n';
 }
