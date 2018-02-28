7b983e2de3e224857b27c52e40f8d4c502d83b6d
OmarSebres <omar.sebres@gmail.com>
2018-02-28 22:14:55 +0100

    solution

---
+++ hacker_earth/algorithms/sorting/merge_sort/merge_sort.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1000000;
+
+int a[N];
+int c[N];
+
+long long sort(int b, int e) {
+    if (b >= e) return 0;
+
+    int m = (b + e) / 2;
+    long long ls = sort(b, m);
+    long long rs = sort(m + 1, e);
+
+    int* t = new int[e - b + 1];
+    int jl = b;
+    int jr = m + 1;
+    int ti = 0;
+
+    long long res = ls + rs;
+    for (int i = b; i <= e; i++) {
+        if (jl > m) {
+            t[ti++] = a[jr++];
+        } else if (jr > e) {
+            t[ti++] = a[jl++];
+        } else if (a[jl] > a[jr]) {
+            t[ti++] = a[jr++];
+            res += (m -  jl + 1LL);
+        } else {
+            t[ti++] = a[jl++];
+        }
+    }
+    for (int i = 0, ai = b; i < ti; i++) {
+        a[ai++] = t[i];
+    }
+    return res;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    long long swaps = sort(0, n - 1);
+    cout << swaps << '\n';
+}
